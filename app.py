ac349e4363618b4c775dacadeacfe1e633e7355e
JESUS MORALES <jesus.alberto.morales.lopez@gmail.com>
2026-06-03 15:44:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -258,14 +258,19 @@ with tab_areas:
                 
                 # 2. Subir de forma automática el archivo actualizado a tu Repositorio de GitHub
                 if GITHUB_TOKEN:
-                    url_api_personal = f"https://github.com{REPO_NAME}/contents/{ARCHIVO_PERSONAL}"
+                    # Dirección de la API oficial formateada con barras diagonales estrictas
+                    url_api_personal = f"https://github.com{ARCHIVO_PERSONAL}"
+                    
                     headers_github = {
                         "Authorization": f"token {GITHUB_TOKEN}",
                         "Accept": "application/vnd.github.v3+json",
                         "User-Agent": "Streamlit-App"
                     }
+                    
+                    # Obtener el SHA del archivo actual en GitHub para poder reemplazarlo
                     sha_personal = None
                     res_get = requests.get(url_api_personal, headers=headers_github, timeout=10)
+
 
 
                     if res_get.status_code == 200:
